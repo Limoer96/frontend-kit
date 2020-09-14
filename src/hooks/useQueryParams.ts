9feb8b13fb5246d25885841e5f03cca18925c3e0
Limoer96 <myt19960503@outlook.com>
2020-09-14 15:05:13 +0800

    [!]修改useQueryParams参数及bug

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -2,25 +2,32 @@ import { Dict } from '../definitions'
 import { useMemo } from 'react'
 
 /**获取路由参数值 */
-export const useQueryParam = (location: Location, paramKey: string) => {
+export const useQueryParam = (search: string, paramKey: string) => {
+  if (!search || !paramKey) {
+    return ''
+  }
   const value = useMemo(() => {
-    const params = new URLSearchParams(location.search)
+    const params = new URLSearchParams(search)
     const value = params.get(paramKey)
     return value
-  }, [location.search])
+  }, [search])
   return value
 }
 /**
  * 获取所有查询参数
  * @param location
  */
-export const useQueryParams = (location: Location) => {
+export const useQueryParams = (search: string) => {
+  if (!search) {
+    return {}
+  }
   const result = useMemo(() => {
-    const params = new URLSearchParams(location.search)
+    const params = new URLSearchParams(search)
     const result: Dict = {}
     params.forEach((val, key) => {
       result[key] = val
     })
-  }, [location.search])
+    return result
+  }, [search])
   return result
 }
