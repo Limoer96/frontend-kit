ed8eb7f601cf00493a5924d15572c4ed4b54d55a
limoer <myt19960503@outlook.com>
2021-02-24 20:31:56 +0800

    fix: usePagination重构

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react'
-
+import warn from '../utils/warning'
 export interface IPagination {
   page: number
   size: number
@@ -13,6 +13,7 @@ const DEFAULT_OPTIONS: IPagination = {
 export function useNoRenderPagination(
   initPageOptions: IPagination = DEFAULT_OPTIONS
 ): [IPagination, (pageOptions: IPagination) => void] {
+  warn.warning('Deprecated: use `usePagination instead.`')
   const { current } = useRef(initPageOptions)
   function setPagination(pageOptions: IPagination) {
     current.page = pageOptions.page
@@ -24,9 +25,36 @@ export function useNoRenderPagination(
   return [pageOptions, setPagination]
 }
 
-export default function (
+const useLazyRenderPagination = (
   pageOption: IPagination = DEFAULT_OPTIONS
+): [number, number, (page: number, size: number) => void, (current: number, size: number) => void, () => void] => {
+  const { current } = useRef(pageOption)
+  function handleChange(page: number, pageSize: number) {
+    current.page = page
+    current.size = pageSize
+  }
+  function handleShowSizeChange(_, size: number) {
+    current.page = pageOption.page
+    current.size = size
+  }
+  function reset() {
+    handleChange(pageOption.page, pageOption.size)
+  }
+  return [current.page, current.size, handleChange, handleShowSizeChange, reset]
+}
+
+/**
+ * 基于 `antd` Pagination组件相关API，对分页器管理的hook
+ * @param pageOption
+ * @param renderOnChange 更改后是否触发更新
+ */
+export default function (
+  pageOption: IPagination = DEFAULT_OPTIONS,
+  renderOnChange = true
 ): [number, number, (page: number, size: number) => void, (current: number, size: number) => void, () => void] {
+  if (!renderOnChange) {
+    return useLazyRenderPagination(pageOption)
+  }
   const [page, setPage] = useState(pageOption.page)
   const [size, setSize] = useState(pageOption.size)
   function handleChange(page: number, pageSize: number) {
